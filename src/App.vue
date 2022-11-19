bf9e3378207256131a5fae97f7c25fa4f9f9a130
developer <dev@ir.js.org>
2022-11-19 15:55:40 +0330

    Add title

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+	<div>
+		<h1>List people who has been killed in Iran</h1>
+		<h2 class="font-weight-light">Some data maybe missing, we still trying to add.</h2>
+	</div>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
